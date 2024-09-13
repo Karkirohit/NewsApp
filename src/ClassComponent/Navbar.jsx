e59df8f0ec6a17156e221ff7d6ac01c29bfa9244
rohit <rskarki25@gmail.com>
2024-09-13 20:03:21 +0530

    https://github.com/Karkirohit/NewsApp.git

--- src/ClassComponent/Navbar.jsx
+++ src/ClassComponent/Navbar.jsx
@@ -18,7 +18,7 @@ export default class Navbar extends Component {
 
     render() {
         return (
-            <nav className="navbar navbar-expand-lg background ">
+            <nav className="navbar navbar-expand-lg background sticky-top ">
                 <div className="container-fluid ">
                     <a className="navbar-brand text-light" href="/">Navbar</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
